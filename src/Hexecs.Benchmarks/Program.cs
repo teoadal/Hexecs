f17f2dc2741c8df894c10855ab16539bbfd6bff9
Kirill Bazhaikin <kirly@yandex.ru>
2025-12-22 13:08:29 +0300

    try add benchmarks

--- src/Hexecs.Benchmarks/Program.cs
+++ src/Hexecs.Benchmarks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Running;
 using Hexecs.Benchmarks.Actors;
 
-BenchmarkRunner.Run<UpdateSystemWithParallelWorkerBenchmark>();
+//BenchmarkRunner.Run<UpdateSystemWithParallelWorkerBenchmark>();
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
